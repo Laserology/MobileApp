05ec046a140e70581054e34379669f70e229e0c2
Laserology <167719528+Laserology@users.noreply.github.com>
2024-04-21 15:23:49 -0700

    Initial code commit.

---
+++ app/src/main/java/com/laserology/android/MainActivity.java
@@ -0,0 +1,219 @@
+package com.laserology.android;
+
+import android.animation.*;
+import android.animation.ObjectAnimator;
+import android.app.*;
+import android.app.AlertDialog;
+import android.content.*;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.webkit.*;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.*;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.*;
+import org.json.*;
+
+public class MainActivity extends AppCompatActivity {
+	
+	private Timer _timer = new Timer();
+	
+	private ArrayList<String> DockList = new ArrayList<>();
+	
+	private LinearLayout linear2;
+	private LinearLayout linear3;
+	private WebView webview1;
+	private ImageView imageview1;
+	
+	private RequestNetwork Network;
+	private RequestNetwork.RequestListener _Network_request_listener;
+	private TimerTask Timer;
+	private ObjectAnimator Animation = new ObjectAnimator();
+	private AlertDialog.Builder UpdatePopup;
+	private Intent UpdateIntent = new Intent();
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear2 = findViewById(R.id.linear2);
+		linear3 = findViewById(R.id.linear3);
+		webview1 = findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+		imageview1 = findViewById(R.id.imageview1);
+		Network = new RequestNetwork(this);
+		UpdatePopup = new AlertDialog.Builder(this);
+		
+		webview1.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				linear2.removeView(webview1);
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				linear2.addView(webview1);
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+		
+		imageview1.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View _view) {
+				linear2.addView(webview1);
+				return true;
+			}
+		});
+		
+		_Network_request_listener = new RequestNetwork.RequestListener() {
+			@Override
+			public void onResponse(String _param1, String _param2, HashMap<String, Object> _param3) {
+				final String _tag = _param1;
+				final String _response = _param2;
+				final HashMap<String, Object> _responseHeaders = _param3;
+				if (Double.parseDouble(_response) > 2) {
+					UpdatePopup.setIcon(R.drawable.logo);
+					UpdatePopup.setTitle("Update available");
+					UpdatePopup.setMessage("An update for this app is available. It is recomended to update, as we've probably added fixes for issues.");
+					UpdatePopup.setPositiveButton("Update ", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface _dialog, int _which) {
+							UpdateIntent.setAction(Intent.ACTION_VIEW);
+							UpdateIntent.setData(Uri.parse("https://laserology.net/Update.apk"));
+							startActivity(UpdateIntent);
+						}
+					});
+					UpdatePopup.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface _dialog, int _which) {
+							
+						}
+					});
+					UpdatePopup.create().show();
+				}
+			}
+			
+			@Override
+			public void onErrorResponse(String _param1, String _param2) {
+				final String _tag = _param1;
+				final String _message = _param2;
+				
+			}
+		};
+	}
+	
+	private void initializeLogic() {
+		Animation.setInterpolator(new LinearInterpolator());
+		Animation.setRepeatMode(ValueAnimator.RESTART);
+		Animation.setPropertyName("rotation");
+		Animation.setTarget(imageview1);
+		Animation.setFloatValues((float)(0), (float)(360));
+		Animation.setRepeatCount((int)(-1));
+		Animation.setDuration((int)(2500));
+		Animation.start();
+		webview1.loadUrl("https://laserology.net/");
+	}
+	
+	@Override
+	public void onBackPressed() {
+		webview1.goBack();
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle _savedInstanceState) {
+		super.onPostCreate(_savedInstanceState);
+		Network.startRequestNetwork(RequestNetworkController.GET, "https://laserology.net/Update.txt", "", _Network_request_listener);
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
